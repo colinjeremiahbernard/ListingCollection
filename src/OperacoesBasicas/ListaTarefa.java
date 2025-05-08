fc0b4298eff947a03b18f529c9ecd04e3a59d06a
Colin Jeremiah Stephenson <colinjeremiahbernard@gmail.com>
2025-05-07 22:56:31 -0400

    Java Operacoes Basicas

---
+++ src/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,46 @@
+package OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    //atributo
+    private final List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+    public void removerTarefa(String descricao) {
+        List<Tarefa> TarefasParaRemover = new ArrayList<>();
+        for (Tarefa t : tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                TarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(TarefasParaRemover);
+    }
+    public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+    }
+    public void obterDescricoesTarefas() {
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("O numero total de elementos na lista e : " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        System.out.println("O numero total de elementos na lista e : " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 1");
+        System.out.println("O numero total de elementos na lista e : " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+    }
+}
